4a6872c791e38dfd18da7936ced1f1c0a7cc3d9b
Swan Chase <16647@ma-web.nl>
2015-01-13 09:01:11 +0100

    working spawn system

--- tower-defense/Assets/Scripts/Enemies/SpawnScript.cs
+++ tower-defense/Assets/Scripts/Enemies/SpawnScript.cs
@@ -6,29 +6,79 @@ using System.Collections;
 public class SpawnScript : MonoBehaviour {
 
     public WaypointTest waypoints;
-    public GameObject[] enemyTypes;
-    public float interval = 3.0f;
-    private int _currentEnemyIndex = -1;
-    private float _timeLeft = 0.0f;
+    //public GameObject[] enemyTypes;
+    //[HideInInspector]
+    //public float interval = 10.0f;
+    //private int _currentEnemyIndex = -1;
+    //private float _timeLeft = 5.0f;
+    //private int _waveCounter = 0;
 
-    GameObject[] enemyWaves;
+    public GameObject[] enemyTypes = new GameObject[0];
+    public Vector3 spawnValues;
+    public int EnemyCount;
+    public float startWait;
+    public float spawnWait;
+    public float waveWait;
 
-    void Update() {
-
-        _timeLeft -= Time.deltaTime;
-        if (_timeLeft <= 0.0f) {
-
-            // enemy sequance
-            _currentEnemyIndex++;
-            if (_currentEnemyIndex >= enemyTypes.Length) _currentEnemyIndex = 0;
-
-            Debug.Log("Spawn Enemy");
-
-            GameObject g = (GameObject)Instantiate(enemyTypes[_currentEnemyIndex], transform.position, Quaternion.identity);
-            g.GetComponent<Enemy>().waypoints = waypoints.wayPoints;
-
-            _timeLeft = interval;
-        }
-
+    void Start()
+    {
+        StartCoroutine(SpawnWaves());
     }
+
+    IEnumerator SpawnWaves()
+    {
+        yield return new WaitForSeconds(startWait);
+        while (true)
+        {
+            for (int i = 0; i < EnemyCount; i++)
+            {
+                Vector3 spawnPostition = new Vector3 (spawnValues.x, spawnValues.y, spawnValues.z);
+                Quaternion spawnRotation = Quaternion.identity;
+                GameObject g = (GameObject)Instantiate(enemyTypes[Random.Range(0, 5)], spawnPostition, spawnRotation);
+                yield return new WaitForSeconds(spawnWait);
+                g.GetComponent<Enemy>().waypoints = waypoints.wayPoints;
+            }
+            yield return new WaitForSeconds(waveWait);
+        }
+    }
+
+    //GameObject[] enemyWaves;
+
+    //void Update() {
+    //    _timeLeft -= Time.deltaTime;
+    //    if (_timeLeft <= 0.0f)
+    //    {
+
+    //        if (_waveCounter >= 5)
+    //        {
+    //            _waveCounter = 0;
+    //        }
+    //        else
+    //        {
+    //            _waveCounter++;
+    //        }
+
+    //        // enemy sequance
+
+    //        Debug.Log("Enemy Spawned");
+
+    //        for (int i = 0; i <= 10; i++)
+    //        {
+    //            StartCoroutine(EnemySpawn(enemyTypes[_waveCounter], transform.position, Quaternion.identity,1f * i ));
+    //            Debug.Log("LALALALALA 2");
+    //        }
+    //        _timeLeft = interval;
+    //    }
+
+    //}
+
+    //IEnumerator EnemySpawn(GameObject enemy, Vector3 position, Quaternion rotation, float seconds)
+    //{
+    //    yield return new WaitForSeconds(seconds);
+    //    Debug.Log("LALALALALA");
+    //    GameObject g = (GameObject)Instantiate(enemy, position, rotation);
+    //    g.GetComponent<Enemy>().waypoints = waypoints.wayPoints;
+        
+    //}
+
 }
